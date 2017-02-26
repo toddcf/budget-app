78c150b326a62abf05f781635afb799ce0bb0f51
Todd Croak-Falen <toddcf@gmail.com>
2017-02-26 14:04:09 -0800

    Removed white spaces between function and parentheses.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,26 @@
 // Budget Controller Module (an IIFE that returns an object):
-var budgetController = ( function () {
+var budgetController = ( function() {
 	var x = 23;
-	var add = function ( a ) {
+	var add = function( a ) {
 		return x + a;
 	};
 	return {
-		publicTest: function ( b ) {
+		publicTest: function( b ) {
 			return ( add ( b ) );
 		}
 	}
 }) ();
 
 // UI Controller Module (also an IIFE):
-var UIController = ( function () {
+var UIController = ( function() {
 
 }) ();
 
 // App Controller that brokers all the different modules, which are passed into it as arguments:
-var controller = ( function ( budgetCtrl , UICtrl ) {
+var controller = ( function( budgetCtrl , UICtrl ) {
 	var z = budgetCtrl.publicTest ( 5 );
 	return {
-		anotherPublic: function () {
+		anotherPublic: function() {
 			console.log ( z );
 		}
 	}
