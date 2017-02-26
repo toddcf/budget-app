f82c52c7fd9dada5df67f8dc1466e996c776cce7
Todd Croak-Falen <toddcf@gmail.com>
2017-02-26 14:00:51 -0800

    Added spaces for readability.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,27 @@
 // Budget Controller Module (an IIFE that returns an object):
-var budgetController = (function() {
+var budgetController = ( function () {
 	var x = 23;
-	var add = function(a) {
+	var add = function ( a ) {
 		return x + a;
 	};
 	return {
-		publicTest: function(b) {
-			return(add(b));
+		publicTest: function ( b ) {
+			return ( add ( b ) );
 		}
 	}
-})();
+}) ();
 
 // UI Controller Module (also an IIFE):
-var UIController = (function() {
+var UIController = ( function () {
 
-})();
+}) ();
 
 // App Controller that brokers all the different modules, which are passed into it as arguments:
-var controller = (function(budgetCtrl, UICtrl) {
-	var z = budgetCtrl.publicTest(5);
+var controller = ( function ( budgetCtrl , UICtrl ) {
+	var z = budgetCtrl.publicTest ( 5 );
 	return {
-		anotherPublic: function() {
-			console.log(z);
+		anotherPublic: function () {
+			console.log ( z );
 		}
 	}
-})(budgetController, UIController);
+}) ( budgetController , UIController );
