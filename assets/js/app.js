fcd761cc35c6045ea751f6e902fc700fbfb6df10
Todd Croak-Falen <toddcf@gmail.com>
2017-03-02 15:42:18 -0800

    Set up event listener for when ENTER is pressed. (No other keys.)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,6 @@
 // Budget Controller Module (an IIFE that returns an object):
 var budgetController = ( function() {
-	var x = 23;
-	var add = function( a ) {
-		return x + a;
-	};
-	return {
-		publicTest: function( b ) {
-			return ( add ( b ) );
-		}
-	}
+	
 }) ();
 
 // UI Controller Module (also an IIFE):
@@ -16,12 +8,21 @@ var UIController = ( function() {
 
 }) ();
 
-// App Controller that brokers all the different modules, which are passed into it as arguments:
+// Global App Controller that communicates between all the other modules, which are passed into it as arguments:
 var controller = ( function( budgetCtrl , UICtrl ) {
-	var z = budgetCtrl.publicTest ( 5 );
-	return {
-		anotherPublic: function() {
-			console.log ( z );
+	// Event listener for button click:
+	document.querySelector(".add__btn").addEventListener("click", function() {
+		// 1. Get field input data.
+		// 2. Add item to budget controller.
+		// 3. Add the new item to the user interface.
+		// 4. Calculate the budget.
+		// 5. Display budget in user interface.
+	});
+
+	// Event listener for ENTER key:
+	document.addEventListener("keypress", function(event) {
+		if ( event.keyCode === 13 || event.which === 13 ) {
+			console.log("ENTER was pressed.");
 		}
-	}
+	});
 }) ( budgetController , UIController );
