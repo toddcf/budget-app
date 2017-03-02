b8b1b23906e2b6059313497eb0e81b0a5c903764
Todd Croak-Falen <toddcf@gmail.com>
2017-03-02 15:49:50 -0800

    Created pseudo code function to be executed with either the keypress or button click.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,19 +10,24 @@ var UIController = ( function() {
 
 // Global App Controller that communicates between all the other modules, which are passed into it as arguments:
 var controller = ( function( budgetCtrl , UICtrl ) {
-	// Event listener for button click:
-	document.querySelector(".add__btn").addEventListener("click", function() {
+
+	// Control Add Item Function to be executed when button or ENTER key is clicked:
+	var ctrlAddItem = function() {
 		// 1. Get field input data.
 		// 2. Add item to budget controller.
 		// 3. Add the new item to the user interface.
 		// 4. Calculate the budget.
 		// 5. Display budget in user interface.
-	});
+		console.log("ctrolAddItem function is working!");
+	};
+
+	// Event listener for button click:
+	document.querySelector(".add__btn").addEventListener("click", ctrlAddItem );
 
 	// Event listener for ENTER key:
 	document.addEventListener("keypress", function(event) {
 		if ( event.keyCode === 13 || event.which === 13 ) {
-			console.log("ENTER was pressed.");
+			ctrlAddItem()
 		}
 	});
 }) ( budgetController , UIController );
