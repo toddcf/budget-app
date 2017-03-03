6df64ada2fea2d953d3f82d422a219f5fac30ef7
Todd Croak-Falen <toddcf@gmail.com>
2017-03-02 22:23:13 -0800

    UI Controller now returns the values as an object. The Global App Controller receives it and console logs the result.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,26 +8,27 @@ var UIController = ( function() {
 	return {
 		getInput: function() {
 			return {
-				type: document.querySelector(".add__type").value; // Will be either inc or exp.
-				description: document.querySelector("add__description").value;
-				value: document.querySelector("add__value").value;
-			}
+				type: document.querySelector(".add__type").value,
+				description: document.querySelector(".add__description").value,
+				value: document.querySelector(".add__value").value
+			};
 		}
 	};
 }) ();
 
-// Global App Controller that communicates between all the other modules, which are passed into it as arguments:
+// Global App Controller IIFE that communicates between all the other modules, which are passed into it as arguments:
 var controller = ( function( budgetCtrl , UICtrl ) {
 
 	// Control Add Item Function to be executed when button or ENTER key is clicked:
 	var ctrlAddItem = function() {
 		// 1. Get field input data.
+		var input = UICtrl.getInput();
+		console.log(input);
 		// 2. Add item to budget controller.
 		// 3. Add the new item to the user interface.
 		// 4. Calculate the budget.
 		// 5. Display budget in user interface.
-		console.log("ctrolAddItem function is working!");
-	};
+	}
 
 	// Event listener for button click:
 	document.querySelector(".add__btn").addEventListener("click", ctrlAddItem );
@@ -35,7 +36,7 @@ var controller = ( function( budgetCtrl , UICtrl ) {
 	// Event listener for ENTER key:
 	document.addEventListener("keypress", function(event) {
 		if ( event.keyCode === 13 || event.which === 13 ) {
-			ctrlAddItem()
+			ctrlAddItem();
 		}
 	});
 }) ( budgetController , UIController );
