50ffb1a0eee0b65ae0b70dbb051a424711d9a9ca
Todd Croak-Falen <toddcf@gmail.com>
2017-03-11 15:07:47 -0800

    Updated displayBudget function to only display the percentage when it's greater than zero; otherwise it inserts dashes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -180,7 +180,13 @@ var UIController = ( function() {
 			document.querySelector( DOMstrings.budgetLabel ).textContent = obj.budget;
 			document.querySelector( DOMstrings.incomeLabel ).textContent = obj.totalInc;
 			document.querySelector( DOMstrings.expensesLabel ).textContent = obj.totalExp;
-			document.querySelector( DOMstrings.percentageLabel ).textContent = obj.percentage;
+			
+			if ( obj.percentage > 0 ) {
+				document.querySelector( DOMstrings.percentageLabel ).textContent = obj.percentage + "%";
+			}
+			else {
+				document.querySelector( DOMstrings.percentageLabel ).textContent = "--";
+			}
 		},
 
 		// Export DOMstrings object for use in the Global App Controller:
